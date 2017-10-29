fea333621fb9f9b1d1bc7b4de25fd7602b01a386
Jayanth Chornur Srinivasa <chornur.srinivasa.jayanth@volvo.com>
2017-10-29 13:26:54 +0530

    Added lite server. Dynamic checking of html and TS changes.

--- app/app.ts
+++ app/app.ts
@@ -2,8 +2,10 @@
  * Created by a028515 on 28-10-2017.
  */
 function startGames() {
+    console.log("This is a New Game");
+    console.log("This is a New Game 2");
     var message  = document.getElementById("messages");
-    message.innerText = "Welcome. Lets play a game !!";
+    message.innerText = "Lets play a game !!. Its your Game !";
 }
 
 
